594939e50a0da092bd440de2ea5cccf93a8a4e6d
Arman Jon Villalobos <me@armanjon.com>
2016-05-26 20:34:46 +0800

    add validation for search result model to avoid blank searches

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,2 +1,3 @@
 class SearchResult < ActiveRecord::Base
+  validates :name, presence: { message: "Must have a search term" }
 end
--- spec/models/search_result_spec.rb
+++ spec/models/search_result_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SearchResult, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with name" do
+    search_result = SearchResult.new(name: "apples")
+    search_result.valid?
+    expect(search_result).to be_truthy
+  end
+
+  it "is invalid without a name" do
+    search_result = SearchResult.new(name: nil)
+    search_result.valid?
+    expect(search_result.errors[:name]).to include("Must have a search term")
+  end
 end
